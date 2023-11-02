901e9e4caa023eb1f459f8e29836a7dc0990ecce
jeshraghian <jeshraghian@gmail.com>
2023-11-02 15:13:10 -0700

    revert to lif

--- src/tt_um_lif.v
+++ src/tt_um_lif.v
@@ -14,10 +14,10 @@ module tt_um_lif (
 
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
-    // assign uio_out[6:0] = 6'd0;
+    assign uio_out[6:0] = 6'd0;
 
     // instantiate lif neuron
-    lif_network lif_network (.current(ui_in), .spike_out(uo_out), .state_out(uio_out), .clk(clk), .rst_n(rst_n));
-
+    lif lif1(.current(ui_in), .clk(clk), .rst_n(rst_n), .spike(uio_out[7]), .state(uo_out));
+    // lif lif2(.current({uio_out[7], 7'b0000000}), .clk(clk), .rst_n(rst_n), .spike(uio_out[6]), .state(uo_out));
 
 endmodule
