aeac92259d10d317e55d80f0e17efc950aec8961
jeshraghian <jeshraghian@gmail.com>
2023-11-02 13:07:20 -0700

    fix indexing of l1 state

--- src/lif_network.v
+++ src/lif_network.v
@@ -10,18 +10,18 @@ module lif_network (
 );
 
     wire [7:0] l1_out; // One output per lif neuron
-    wire [7:0] l1_state;
-    reg [7:0] sum; // register to hold the summation
+    wire [63:0] l1_state;
+    reg  [7:0] sum; // register to hold the summation
 
     //Instantiate 8 lif neurons
-    lif lif1(.current(current[0]), .clk(clk), .rst_n(rst_n), .spike(l1_out[0]), .state(l1_state[0]));
-    lif lif2(.current(current[1]), .clk(clk), .rst_n(rst_n), .spike(l1_out[1]), .state(l1_state[1]));
-    lif lif3(.current(current[2]), .clk(clk), .rst_n(rst_n), .spike(l1_out[2]), .state(l1_state[2]));
-    lif lif4(.current(current[3]), .clk(clk), .rst_n(rst_n), .spike(l1_out[3]), .state(l1_state[3]));
-    lif lif5(.current(current[4]), .clk(clk), .rst_n(rst_n), .spike(l1_out[4]), .state(l1_state[4]));
-    lif lif6(.current(current[5]), .clk(clk), .rst_n(rst_n), .spike(l1_out[5]), .state(l1_state[5]));
-    lif lif7(.current(current[6]), .clk(clk), .rst_n(rst_n), .spike(l1_out[6]), .state(l1_state[6]));
-    lif lif8(.current(current[7]), .clk(clk), .rst_n(rst_n), .spike(l1_out[7]), .state(l1_state[7]));
+    lif lif1(.current(current[0]), .clk(clk), .rst_n(rst_n), .spike(l1_out[0]), .state(l1_state[0:7]));
+    lif lif2(.current(current[1]), .clk(clk), .rst_n(rst_n), .spike(l1_out[1]), .state(l1_state[8:15]));
+    lif lif3(.current(current[2]), .clk(clk), .rst_n(rst_n), .spike(l1_out[2]), .state(l1_state[16:23]));
+    lif lif4(.current(current[3]), .clk(clk), .rst_n(rst_n), .spike(l1_out[3]), .state(l1_state[24:31]));
+    lif lif5(.current(current[4]), .clk(clk), .rst_n(rst_n), .spike(l1_out[4]), .state(l1_state[32:39]));
+    lif lif6(.current(current[5]), .clk(clk), .rst_n(rst_n), .spike(l1_out[5]), .state(l1_state[40:47]));
+    lif lif7(.current(current[6]), .clk(clk), .rst_n(rst_n), .spike(l1_out[6]), .state(l1_state[48:55]));
+    lif lif8(.current(current[7]), .clk(clk), .rst_n(rst_n), .spike(l1_out[7]), .state(l1_state[56:63]));
 
     // Summing logic 
 
@@ -29,8 +29,8 @@ module lif_network (
         if (!rst_n) begin
             sum <= 0;
         end else begin
-            sum <= l1_out[0] + l1_out[1] + l1_out[3] + l1_out[4] + l1_out[5] +
-            l1_out[6] + l1_out[7];
+            sum <= l1_out[0:7] + l1_out[8:15] + l1_out[16:23] + l1_out[24:31] + l1_out[32:39] +
+            l1_out[40:47] + l1_out[48:55] + l1_out[56:63];
         end
     end
 
