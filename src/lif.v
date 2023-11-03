c2c54798e871efd4b729e478cc867a5ce7261179
jeshraghian <jeshraghian@gmail.com>
2023-11-03 15:25:21 -0700

    fix negative shit

--- src/lif.v
+++ src/lif.v
@@ -11,7 +11,7 @@ module hh  ( //#(parameter EXP = 8'b0010_1011)
     reg [7:0] threshold, n_var, m_var, h_var;//VK, VNa, Vl
     wire [7:0] next_state, current, next_n, next_m, next_h;
     
-    assign current = stim_current - (((m_var**3)*h_var*(state - -50)) >> 3) - (((n_var**4)*(state - 77)) >> 4) - ((state - 54) >> 2);
+    assign current = stim_current - (((m_var**3)*h_var*(state - 50)) >> 3) - (((n_var**4)*(state - 77)) >> 4) - ((state - 54) >> 2);
     assign next_state = (spike[0] ? 0 : (state)) + (current >> 2);
     assign spike = {7'b0, state>= threshold};
     assign next_n = n_var + (((state*(1-n_var)) >> 2 - (state*n_var) >> 2) >> 2); // replace states w/ alpha(state)
