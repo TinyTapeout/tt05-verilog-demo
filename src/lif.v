e9bf5126e7fa135043ee891543cac6107183bb2e
jeshraghian <jeshraghian@gmail.com>
2023-11-03 15:29:01 -0700

    remove power ops

--- src/lif.v
+++ src/lif.v
@@ -10,8 +10,10 @@ module hh  ( //#(parameter EXP = 8'b0010_1011)
     //reg [7:0] next_state, threshold, current, INa, IK, IKleak, m_alph, m_beta, m_act, h_alph, h_beta, h_act, n_alph, n_beta, n_act;
     reg [7:0] threshold, n_var, m_var, h_var;//VK, VNa, Vl
     wire [7:0] next_state, current, next_n, next_m, next_h;
-    
-    assign current = stim_current - (((m_var**3)*h_var*(state - 50)) >> 3) - (((n_var**4)*(state - 77)) >> 4) - ((state - 54) >> 2);
+
+    // assign current = stim_current - (((m_var**3)*h_var*(state - 50)) >> 3) - (((n_var**4)*(state - 77)) >> 4) - ((state - 54) >> 2);
+
+    assign current = stim_current - (((m_var*3)*h_var*(state - 50)) >> 3) - (((n_var*4)*(state - 77)) >> 4) - ((state - 54) >> 2);
     assign next_state = (spike[0] ? 0 : (state)) + (current >> 2);
     assign spike = {7'b0, state>= threshold};
     assign next_n = n_var + (((state*(1-n_var)) >> 2 - (state*n_var) >> 2) >> 2); // replace states w/ alpha(state)
